a0539d831f68e379ff6e1c5565ea0ee921b1ba46
BSH <bash_1997@outlook.com>
2021-07-17 23:49:54 -0400

    init commit & assignment completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 const randomFolks = document.querySelector(".random-peeps");
+const getData = async function() {
+    const userResquest = await fetch(
+        URL = "https://randomuser.me/api?results=5"
+    );
+    const data = await userResquest.json();
+    //Once you have found the property name for the array of objects, 
+    //create a new variable and map it to that property
+    const usersResults = data["results"];
+    displayUsers(usersResults);
+}
+
+getData();
+
+const displayUsers = async function(usersResults) {
+    randomFolks.innerHTML = "";
+    
+    for (let user = 0; user < usersResults.length; user++) {
+        const userIndex = usersResults[user];
+        const country = userIndex["location"]["country"];
+        const name = userIndex["name"]["first"];
+        const imageUrl = userIndex["picture"]["medium"];
+
+        const userDiv = document.createElement("DIV");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar"/>
+        `;
+        randomFolks.append(userDiv);
+    }
+
+    
+    
+}
